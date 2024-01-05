791fdfcdc1422ff734eb7a7e04b0c9d699542d2c
quangtn266 <quangtrandn93@gmail.com>
2024-01-05 19:53:15 +0700

    Update 'Ensemble Model Serving Pattern'

---
+++ EnsembleModelServingPattern/major_class_finder.py
@@ -0,0 +1,11 @@
+from collections import Counter
+
+x = ['C1', 'C1', 'C1', 'C2', 'C2', 'C3', 'C3']
+
+counts = Counter(x)
+
+print("Count of different elements", counts)
+
+major_element = counts.most_common(1)[0][0]
+
+print("Major element", major_element)
